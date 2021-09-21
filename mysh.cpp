13008c3ac5fc1cfa4fded41616dabb7ddb0ba321
Greg <gregfreitas1997@gmail.com>
2021-09-21 17:05:18 -0400

    Writing outline for the assignment

---
+++ mysh.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main () {
+
+    return 0;
+}
+
+void move_to_dir()
+{
+
+}
+
+void where_am_i()
+{
+
+}
+
+void history()
+{
+
+}
+
+void bye_bye()
+{
+
+}
+
+void replay_number()
+{
+
+}
+
+void start_program()
+{
+    
+}
+
+void background_program()
+{
+
+}
+
+void dalek()
+{
+
+}
+
+void repeat()
+{
+
+}
+
+void dalek_all()
+{
+
+}
